bab5a212ddcdcca8a9632a45e36ff6ba82aad776
rodleyorosa <rodleyorosa@gmail.com>
2025-07-05 16:43:33 +0200

    chore: remove / from base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/rodleyorosa-site',
+  base: 'rodleyorosa-site',
   plugins: [react(), tailwindcss()],
 });
