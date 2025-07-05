d3ecdd89050bd86463646503934370bddc113552
rodleyorosa <rodleyorosa@gmail.com>
2025-07-05 20:51:11 +0200

    style: change font family

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,13 +3,13 @@ export const Footer = (): React.JSX.Element => {
     <div className="bg-[#151515]">
       <div className="w-[80%] mx-auto max-w-[40rem] py-[5rem]">
         <div className="text-center md:flex md:justify-center">
-          <p className="text-sm font-normal leading-[1.875rem] text-[#888] after:content-['|'] after:px-4 md:inline-block">
+          <p className="font-poppins text-sm leading-[1.875rem] text-[#888] md:after:content-['|'] after:px-4 md:inline-block">
             &copy; Copyright 2025
           </p>
-          <p className="text-sm font-normal leading-[1.875rem] text-[#888] after:content-['|'] after:px-4 md:inline-block">
+          <p className="font-poppins text-sm font-normal leading-[1.875rem] text-[#888] md:after:content-['|'] after:px-4 md:inline-block">
             Developed by <span className="text-white">Rodley Orosa</span>
           </p>
-          <p className="text-sm font-normal leading-[1.875rem] text-[#888]">
+          <p className="font-poppins text-sm font-normal leading-[1.875rem] text-[#888]">
             Built with React Typescript
           </p>
         </div>
